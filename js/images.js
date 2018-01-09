2677db2dd902e39a55605791ff636970230ca67b
Marcin Wladymiruk <marcin.wladymiruk@gmail.com>
2018-01-09 12:25:24 +0100

    Done with some bugs

--- js/images.js
+++ js/images.js
@@ -1,22 +1,22 @@
-if ($(window).width > 320){
-    $("#photo1").hover(function(){
-            $("#photo1").animate({height: '100%',width: '100%'});
-        },function(){
-            $("#photo1").animate({height: '80%',width: '80%'});
+if (window.screen.width > 320) {
+    $("#photo1").hover(function () {
+            $("#photo1").animate({height: '100%', width: '100%'});
+        }, function () {
+            $("#photo1").animate({height: '80%', width: '80%'});
         }
     );
 
-    $("#photo2").hover(function(){
-            $("#photo2").animate({height: '100%',width: '100%'});
-        },function(){
-            $("#photo2").animate({height: '80%',width: '80%'});
+    $("#photo2").hover(function () {
+            $("#photo2").animate({height: '100%', width: '100%'});
+        }, function () {
+            $("#photo2").animate({height: '80%', width: '80%'});
         }
     );
 
-    $("#photo3").hover(function(){
-            $("#photo3").animate({height: '100%',width: '100%'});
-        },function(){
-            $("#photo3").animate({height: '80%',width: '80%'});
+    $("#photo3").hover(function () {
+            $("#photo3").animate({height: '100%', width: '100%'});
+        }, function () {
+            $("#photo3").animate({height: '80%', width: '80%'});
         }
     );
 }
