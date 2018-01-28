6ef9508e6cae1a3336e4f4ee4b58827696ede972
Marcin Wladymiruk <marcin.wladymiruk@gmail.com>
2018-01-28 18:26:38 +0100

    For Monday

--- js/images2.js
+++ js/images2.js
@@ -1,15 +1,19 @@
-function enlarge(){
-    $(this).animate({height: '100%', width: '100%'});
-}
-function shrink() {
-    $(this).animate({height: '80%', width: '80%'});
-}
+$(window).on('resize', function() {
+    $windowWidth = $(this).width();
 
-$("#photo1").on((window.screen.width > 320) && "hover", $("#photo1").hover(enlarge,shrink)
-);
+    if ($windowWidth > 320){
+        function enlarge(){
+            $(this).animate({height: '100%', width: '100%'});
+        }
+        function shrink() {
+            $(this).animate({height: '80%', width: '80%'});
+        }
 
-$("#photo2").on((window.screen.width > 320) && "hover", $("#photo2").hover(enlarge,shrink)
-);
+        $("#photo1").hover(enlarge,shrink);
 
-$("#photo3").on((window.screen.width > 320) && "hover", $("#photo3").hover(enlarge,shrink)
-);
+        $("#photo2").hover(enlarge,shrink);
+
+        $("#photo3").hover(enlarge,shrink);
+    }
+    console.log($windowWidth);
+});
