42348f6fbf86a0df89a6922dbdae96ccca17e66e
Marcin <marcin.wladymiruk@gmail.com>
2018-02-02 14:31:45 +0100

    Bootstrap fixed

--- js/images2.js
+++ js/images2.js
@@ -1,19 +1,20 @@
-$(window).on('resize', function() {
-    $windowWidth = $(this).width();
-
-    if ($windowWidth > 320){
-        function enlarge(){
-            $(this).animate({height: '100%', width: '100%'});
-        }
-        function shrink() {
-            $(this).animate({height: '80%', width: '80%'});
-        }
-
-        $("#photo1").hover(enlarge,shrink);
-
-        $("#photo2").hover(enlarge,shrink);
-
-        $("#photo3").hover(enlarge,shrink);
-    }
-    console.log($windowWidth);
-});
+// $(window).on('resize', function() {
+//     $windowWidth = $(this).width();
+//
+//     if ($windowWidth > 320){
+//         function enlarge(){
+//             $(this).animate({height: '100%', width: '100%'});
+//         }
+//         function shrink() {
+//             $(this).animate({height: '80%', width: '80%'});
+//         }
+//
+//         $("#photo1").hover(enlarge,shrink);
+//
+//         $("#photo2").hover(enlarge,shrink);
+//
+//         $("#photo3").hover(enlarge,shrink);
+//     }
+//     console.log($windowWidth);
+// });
+//
